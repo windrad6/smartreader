3ff181fd1c09e8a6bae1a3337559ff7cb984c677
Manuel <manuel@pitz.cc>
2020-04-12 12:26:44 +0200

    single handler for all attrs

--- smartRead.sh
+++ smartRead.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
 
 
-handle_Temperature () {
+handle_Type () {
 	local vendor=$1
+	local attrName=$2
 	local data=$@
-	temp=`grep "Temperature_Celsius" <<< "$data" | tr -s ' ' | cut -d" " -f10 | sed "s/^[ \t]*//"`	
+	temp=`grep "$attrName" <<< "$data" | tr -s ' ' | cut -d" " -f10 | sed "s/^[ \t]*//"`	
 	echo $temp
 }
 
 
 
 
-echo "readSmaprtData"
-
-DRIVES=(`smartctl --scan | cut -d" " -f1`)
+echo "readSmartData"
 
 
-for drive in "${DRIVES[@]}" 
+DRIVES=(`smartctl --scan`)
+
+
+for drive in "${DRIVES[*]}" 
 do
-	driveData=`smartctl -a $drive`
+	
+	path=`cut -d" " -f1 <<< "$drive"`
+	devType=`cut -d" " -f6 <<< "$drive"`
+	
+	driveData=`smartctl -a $path`
 	driveFamily=`grep "Model Family" <<< "$driveData" | tr -s ' ' | cut -d":" -f2 | sed "s/^[ \t]*//"`	
 	driveModel=`grep "Device Model" <<< "$driveData" | tr -s ' ' | cut -d":" -f2 | sed "s/^[ \t]*//"`	
 	driveSerial=`grep "Serial Number" <<< "$driveData" | tr -s ' ' | cut -d":" -f2 | sed "s/^[ \t]*//"`	
 	
 	#echo $driveName	
 	vendor=`cut -d" " -f1 <<< "$driveFamily"`
+	
+	if [ $devType != "ATA" ]; then
+		echo "Device type $devType not supported yet"
+		continue
+	fi
+
 	if [ $vendor = "Seagate" ]; then
-		echo "rerun smartctl"
+		echo "rerun smartctl for Seagate drives"
 		driveData=`smartctl -a -v 7,raw48:54 -v 1,raw48:54 $drive`
 	fi
-	temp=$(handle_Temperature $vendor "$driveData")
+
+	temp=$(handle_Temperature $vendor "Temperature_Celsius" "$driveData")
 	echo "$driveFamily $driveModel $driveSerial $temp"
 done
