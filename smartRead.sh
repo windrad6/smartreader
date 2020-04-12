ffe142f88b5828a966cf6eac8d88e225dcb1bf08
Manuel <manuel@pitz.cc>
2020-04-12 11:02:16 +0200

    get drive data

--- smartRead.sh
+++ smartRead.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+
+handle_Temperature () {
+	vendor=$1
+	data=$2
+	echo "temp handle$vendor"
+	temperature=`grep "Temperature" <<< "$data" | cut -d" " -f10 | sed "s/^[ \t]*//"`	
+}
+
+
+
+
 echo "readSmaprtData"
 
 DRIVES=(`smartctl --scan | cut -d" " -f1`)
@@ -7,5 +18,12 @@ DRIVES=(`smartctl --scan | cut -d" " -f1`)
 
 for drive in "${DRIVES[@]}" 
 do
-	echo "drivname:$drive"
+	driveData=`smartctl -a $drive`
+	
+	driveName=`grep "Model" <<< "$driveData" | cut -d":" -f2 | sed "s/^[ \t]*//"`	
+	
+	echo $driveName	
+	vendor=`cut -d" " -f1 <<< "$driveName"`
+
+	handle_Temperature $vendor $driveData
 done
