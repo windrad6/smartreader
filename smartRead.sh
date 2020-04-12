9e4a43f06136d23d53ee408e832b0cae0c60ec6e
root <root@freenas.local>
2020-04-12 04:33:59 -0700

    first functioning version

--- smartRead.sh
+++ smartRead.sh
@@ -4,8 +4,8 @@
 handle_Type () {
 	local vendor=$1
 	local attrName=$2
-	local data=$@
-	temp=`grep "$attrName" <<< "$data" | tr -s ' ' | cut -d" " -f10 | sed "s/^[ \t]*//"`	
+	local data=$3[@]
+	temp=`grep $attrName <<< "$data" | sed "s/^[ \t]*//" | tr -s ' ' | cut -d" " -f10 | sed "s/^[ \t]*//"`	
 	echo $temp
 }
 
@@ -14,13 +14,11 @@ handle_Type () {
 
 echo "readSmartData"
 
+readarray -t DRIVES <<<`smartctl --scan`
 
-DRIVES=(`smartctl --scan`)
-
-
-for drive in "${DRIVES[*]}" 
+echo -e "Type\t\t\tModel\t\t\tSerial\t\tTemperature\tSeek Err\tRead Err\tUptime" 
+for drive in "${DRIVES[@]}" 
 do
-	
 	path=`cut -d" " -f1 <<< "$drive"`
 	devType=`cut -d" " -f6 <<< "$drive"`
 	
@@ -38,10 +36,14 @@ do
 	fi
 
 	if [ $vendor = "Seagate" ]; then
-		echo "rerun smartctl for Seagate drives"
-		driveData=`smartctl -a -v 7,raw48:54 -v 1,raw48:54 $drive`
+		#echo "rerun smartctl for Seagate drives"
+		driveData=`smartctl -a -v 7,raw48:54 -v 1,raw48:54 $path`
 	fi
 
-	temp=$(handle_Temperature $vendor "Temperature_Celsius" "$driveData")
-	echo "$driveFamily $driveModel $driveSerial $temp"
+	temp=$(handle_Type $vendor "Temperature_Celsius" "$driveData")
+	seek_err=$(handle_Type $vendor "Seek_Error_Rate" "$driveData")
+	read_err=$(handle_Type $vendor "Raw_Read_Error_Rate" "$driveData")
+	power_on=$(handle_Type $vendor "Power_On_Hours" "$driveData")
+
+	echo -e "$driveFamily\t$driveModel\t$driveSerial\t$temp\t\t$seek_err\t\t$read_err\t\t$power_on"
 done
